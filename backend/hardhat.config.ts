f6885ca23ed00bd28336b9feeb142784b8208f61
Vitaly Bobin <vetbobin@gmail.com>
2022-07-31 02:50:41 +0200

    move harhat contracts into separate folder `backend`

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -10,6 +10,14 @@ const KEY: any = process.env.PRIVATE_KEY;
 const config: HardhatUserConfig = {
   networks: {
     hardhat: {},
+    bsc: {
+      url: "https://bsc-dataseed1.binance.org",
+      accounts: [KEY],
+    },
+    bsc_test: {
+      url: "https://data-seed-prebsc-2-s1.binance.org:8545",
+      accounts: [KEY],
+    },
     aurora: {
       url: `https://mainnet.aurora.dev`,
       accounts: [KEY],
---
+++ backend/scripts/deploy.ts
@@ -0,0 +1,16 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Passdapp = await ethers.getContractFactory("Passdapp");
+  const passdapp = await Passdapp.deploy();
+
+  await passdapp.deployed();
+
+  console.log("Lock with 1 ETH deployed to:", passdapp.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+});
